8b0df5d36b90e1ca2e075ccdd78f2b4b9e141db6
Yulka Vdovina <yulka6914@gmail.com>
2026-04-01 14:03:44 +0300

    Fix review comments for menu, header hover, and compare images

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,6 +1,18 @@
 'use strict';
 
-// TODO: edit it
+const fs = require('fs');
+const path = require('path');
+
 describe('Page', () => {
-  it('should be visitable', () => {});
+  it('should be visitable', () => {
+    const pagePath = path.join(__dirname, '..', '..', 'index.html');
+    const html = fs.readFileSync(pagePath, 'utf8');
+
+    expect(fs.existsSync(pagePath)).toBe(true);
+    expect(html).toContain('<title>MYBIKE</title>');
+    expect(html).toContain('Take the Streets');
+    expect(html).toContain('Compare Bikes');
+    expect(html).toContain('The Details');
+    expect(html).toContain('Contact us');
+  });
 });
